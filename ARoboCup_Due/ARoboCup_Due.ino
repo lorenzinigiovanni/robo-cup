6c1db166a1887af29fd05118d71038e57ee6dd92
Giovanni Lorenzini <gioam.lorenzini@gmail.com>
2016-01-24 19:50:17 +0100

    Added and Modified Some Function
    The Avanzamento function now use PID
    The Rotazione function was semplified
    New function: Temperatura, Sensore Temperatura, Servo Torretta, Distanza, Sensore Distanza
    The motor now is controlled directly by the L298 with Motori function

--- ARoboCup_Due/ARoboCup_Due.ino
+++ ARoboCup_Due/ARoboCup_Due.ino
@@ -1,3 +1,15 @@
+//-------------------------------------------------------------------------------
+//PIN
+
+#define M1P 2
+#define M1D 3
+#define M2P 4
+#define M2D 5
+#define M1E 6
+#define M2E 7
+
+//-------------------------------------------------------------------------------
+
 #include <PID_v1.h>
 #include "I2Cdev.h"
 #include "MPU6050_6Axis_MotionApps20.h"
@@ -22,6 +34,15 @@ int numeroMorto = 0;
 //-------------------------------------------------------------------------------
 
 void setup() {
+  pinMode(M1P, OUTPUT);
+  pinMode(M1D, OUTPUT);
+  pinMode(M2P, OUTPUT);
+  pinMode(M2D, OUTPUT);
+  pinMode(M1E, OUTPUT);
+  pinMode(M2E, OUTPUT);
+
+  //-------------------------------------------------------------------------------
+
 #if I2CDEV_IMPLEMENTATION == I2CDEV_ARDUINO_WIRE
   Wire.begin();
   TWBR = 24; // 400kHz I2C clock (200kHz if CPU is 8MHz). Comment this line if having compilation difficulties with TWBR.
@@ -30,7 +51,7 @@ void setup() {
 #endif
 
   Serial.begin(115200);
-  
+
   mpu.initialize();
   devStatus = mpu.dmpInitialize();
   //-2866  -1038 1061  91  -48 12
@@ -46,8 +67,6 @@ void setup() {
     dmpReady = true;
     packetSize = mpu.dmpGetFIFOPacketSize();
   }
-
-  avanzamento(100, 60);
 }
 
 //-------------------------------------------------------------------------------
@@ -57,92 +76,57 @@ void loop() {
 }
 
 //-------------------------------------------------------------------------------
+//MOVIMENTAZIONE
 
-void avanzamentoPID(float distanzaVoluta, float velocita) {
-  float gradiIniziali = gyroscope(0);
+void avanzamento(float distanzaVoluta, float velocita, bool pid) {
+  double gradiIniziali = gyroscope(0);
   float correzione = 0;
-  float distanzaIniziale = distanza();
+  float distanzaIniziale = distanza(0);
   float errore = 0;
-  double Setpoint, Input, Output;
-  double Kp = 2, Ki = 5, Kd = 1;
-  PID pidAvanzamento(&Input, &Output, &Setpoint, Kp, Ki, Kd, DIRECT);
-  pidAvanzamento.SetMode(AUTOMATIC);
+  double giroscopio0 = gyroscope(0);
+  double potenzaM1 = 0;
+  double potenzaM2 = 0;
 
-  Serial.print("start deg: ") && Serial.println(gradiIniziali);
+  if (pid) {
+    double Kp = 2, Ki = 5, Kd = 1;
+    PID pidM1(&giroscopio0, &potenzaM1, &gradiIniziali, Kp, Ki, Kd, DIRECT);
+    PID pidM2(&giroscopio0, &potenzaM2, &gradiIniziali, Kp, Ki, Kd, DIRECT);
+    pidM1.SetMode(AUTOMATIC);
+    pidM2.SetMode(AUTOMATIC);
 
-  if (gradiIniziali < 90) {
-    gradiIniziali += 90;
-    correzione = 90;
-  }
-  else if (gradiIniziali > 270) {
-    gradiIniziali -= 90;
-    correzione = 90;
-  }
-
-  Serial.print("yaw: ") && Serial.println(gradiIniziali);
-
-  while (distanzaIniziale - distanza() <= distanzaVoluta) {
-    pidAvanzamento.Compute();
-    errore = gradiIniziali - gyroscope(0);
-    Serial.print("errore: ") && Serial.print(errore) && Serial.print("\t");
-    Serial.print("delta D: ") && Serial.println(distanzaIniziale - distanza());
-    seriale (velocita - errore, velocita + errore, numeroMorto);
-  }
-
-  seriale (0, 0, numeroMorto);
-}
-
-//-------------------------------------------------------------------------------
-
-void avanzamento(float distanzaVoluta, float velocita) {
-  float gradiIniziali = gyroscope(0);
-  float correzione = 0;
-  //  float distanzaIniziale = distanza();
-  float errore = 0;
-
-  //  if (gradiIniziali < 90) {
-  //    gradiIniziali += 90;
-  //    correzione = 90;
-  //  }
-  //  else if (gradiIniziali > 270) {
-  //    gradiIniziali -= 90;
-  //    correzione = 90;
-  //  }
-
-  //while (distanzaIniziale - distanza() <= distanzaVoluta) {
-  while (true) {
-    errore = gradiIniziali - gyroscope(0);
-//    Serial.println(gyroscope(0));
-    seriale (velocita + errore, velocita - errore, numeroMorto);
-  }
-
-  seriale (0, 0, numeroMorto);
-}
-
-//-------------------------------------------------------------------------------
-
-void rotazione(float gradiVoluti, float velocita) {
-  float gradiIniziali = gyroscope(0);
-
-  if (gradiIniziali > 180) {
-    float gradiFinali = gradiIniziali - gradiVoluti;
-    while (gradiFinali <= gyroscope(0)) {
-      float errore = gradiFinali - gyroscope(0);
-      seriale (-velocita, velocita, numeroMorto);
+    while (distanzaIniziale - distanza(0) <= distanzaVoluta) {
+      giroscopio0 = gyroscope(0);
+      pidM1.Compute();
+      pidM2.Compute();
+      motori (velocita + potenzaM1, velocita - potenzaM2);
     }
+
+    pidM1.SetMode(MANUAL);
+    pidM2.SetMode(MANUAL);
   }
 
   else {
-    float gradiFinali = gradiIniziali + gradiVoluti;
-    while (gradiFinali >= gyroscope(0)) {
-      seriale (velocita, -velocita, numeroMorto);
+    while (distanzaIniziale - distanza(0) <= distanzaVoluta) {
+      errore = gradiIniziali - gyroscope(0);
+      motori (velocita + errore, velocita - errore);
     }
   }
 
-  seriale (0, 0, numeroMorto);
+  motori (0, 0);
+}
+
+void rotazione(float gradiVoluti, float velocita) {
+  float gradiIniziali = gyroscope(0);
+  float gradiFinali = gradiIniziali + gradiVoluti;
+
+  while (gradiFinali >= gyroscope(0))
+    motori (velocita, -velocita);
+
+  motori (0, 0);
 }
 
 //-------------------------------------------------------------------------------
+//GIROSCOPIO
 
 float gyroscope(int scelta) {
   while (fifoCount < packetSize) fifoCount = mpu.getFIFOCount();
@@ -156,14 +140,86 @@ float gyroscope(int scelta) {
 }
 
 //-------------------------------------------------------------------------------
+//SENSORI TORRETTA
 
-float distanza() {
+float temperatura(int gradiMisura) {
+  int servo = 90;
+  servo += gradiMisura;
+
+  while (!servoTorreta(servo)) {}
+
+  float misura = sensoreTemperatura ();
+  return misura;
+}
+
+float distanza(int gradiMisura) {
+  int numeroSensore = 0;
+  int servo = 90;
+  if (gradiMisura <= -90) {
+    numeroSensore = -1;
+    servo += gradiMisura + 90;
+  }
+  else if (gradiMisura >= 90) {
+    numeroSensore = 1;
+    servo += gradiMisura - 90;
+  }
+  else {
+    numeroSensore = 0;
+    servo += gradiMisura;
+  }
+
+  while (!servoTorreta(servo)) {}
+
+  float misura = sensoreDistanza (numeroSensore);
+  return misura;
+}
+
+float sensoreDistanza (int numeroSensore) {
   return 1;
 }
 
+float sensoreTemperatura () {
+  return 1;
+}
+
+bool servoTorreta (int gradi) {
+  return true;
+}
+
 //-------------------------------------------------------------------------------
+//SERIALE VERSO SECONDO ARDUINO
 
 void seriale(float powerMotoreA, float powerMotoreB, int numeroMorto) {
   Serial.print((int)powerMotoreA) && Serial.print(",") && Serial.print((int)powerMotoreB) && Serial.print(";") && Serial.println(numeroMorto);
 }
 
+//-------------------------------------------------------------------------------
+//GESTIONE MOTORI
+
+void motori(int powerM1, int powerM2) {
+  if (powerM1 > 0) {
+    analogWrite (M1D, 0);
+    analogWrite (M1P, powerM1);
+    digitalWrite (M1E, HIGH);
+  }
+  else if (powerM1 < 0) {
+    analogWrite (M1D, 1);
+    analogWrite (M1P, powerM1);
+    digitalWrite (M1E, HIGH);
+  }
+  else
+    digitalWrite (M1E, LOW);
+
+  if (powerM2 > 0) {
+    analogWrite (M2D, 0);
+    analogWrite (M2P, powerM2);
+    digitalWrite (M2E, HIGH);
+  }
+  else if (powerM2 < 0) {
+    analogWrite (M2D, 1);
+    analogWrite (M2P, powerM2);
+    digitalWrite (M2E, HIGH);
+  }
+  else
+    digitalWrite (M2E, LOW);
+}
