d62222912e0d3d747de0845e526619deda6909b8
Giovanni Lorenzini <gioam.lorenzini@gmail.com>
2016-03-05 10:36:09 +0100

    SRF10 example improvement

--- Sensori/SRF10/SRF10.ino
+++ Sensori/SRF10/SRF10.ino
@@ -3,19 +3,24 @@
 #include <Wire.h>
 
 void setup() {
-  Wire.begin();                // join i2c bus (address optional for master)
-  Serial.begin(9600);          // start serial communication at 9600bps
+  Wire.begin();
+  Serial.begin(9600);
+  delay(1000);
+  changeAddress(112, 226); //aggiungere 1-2-3... a 112 e moltiplicare per due
 }
 
-int reading = 0;
-
 void loop() {
+  lettura(0x71);
+}
+
+void lettura(byte address) {
+  int reading = 0;
   // step 1: instruct sensor to read echoes
-  Wire.beginTransmission(112); // transmit to device #112 (0x70)
+  Wire.beginTransmission(address); // transmit to device #112 (0x70)
   // the address specified in the datasheet is 224 (0xE0)
   // but i2c adressing uses the high 7 bits so it's 112
   Wire.write(byte(0x00));      // sets register pointer to the command register (0x00)
-  Wire.write(byte(0x50));      // command sensor to measure in "inches" (0x50)
+  Wire.write(byte(0x51));      // command sensor to measure in "inches" (0x50)
   // use 0x51 for centimeters
   // use 0x52 for ping microseconds
   Wire.endTransmission();      // stop transmitting
@@ -24,12 +29,12 @@ void loop() {
   delay(70);                   // datasheet suggests at least 65 milliseconds
 
   // step 3: instruct sensor to return a particular echo reading
-  Wire.beginTransmission(112); // transmit to device #112
+  Wire.beginTransmission(address); // transmit to device #112
   Wire.write(byte(0x02));      // sets register pointer to echo #1 register (0x02)
   Wire.endTransmission();      // stop transmitting
 
   // step 4: request reading from sensor
-  Wire.requestFrom(112, 2);    // request 2 bytes from slave device #112
+  Wire.requestFrom(address, 2);    // request 2 bytes from slave device #112
 
   // step 5: receive reading from sensor
   if (2 <= Wire.available()) { // if two bytes were received
@@ -42,14 +47,7 @@ void loop() {
   delay(250);                  // wait a bit since people have to read the output :)
 }
 
-
-/*
-
-// The following code changes the address of a Devantech Ultrasonic Range Finder (SRF10 or SRF08)
-// usage: changeAddress(0x70, 0xE6);
-
-void changeAddress(byte oldAddress, byte newAddress)
-{
+void changeAddress(byte oldAddress, byte newAddress) {
   Wire.beginTransmission(oldAddress);
   Wire.write(byte(0x00));
   Wire.write(byte(0xA0));
@@ -70,5 +68,3 @@ void changeAddress(byte oldAddress, byte newAddress)
   Wire.write(newAddress);
   Wire.endTransmission();
 }
-
-*/
